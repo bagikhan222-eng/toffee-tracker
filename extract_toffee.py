320b7a700ccb330b3acd47ccd01442a3a3160b48
bagikhan222-eng <bagikhan222@gmail.com>
2026-06-30 14:14:36 +0600

    Update extract_toffee.py

--- extract_toffee.py
+++ extract_toffee.py
@@ -8,10 +8,26 @@ def run_extraction():
     # 1. The stream configuration
     channel_name = "FIFA Live 6 (2026)"
     original_url = (
-        "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-6/index.m3u8"
-        "?edge-cache-token=Expires=1782802409~Starts=1782802109"
+        "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026/sst/index.m3u8"
+        "?edge-cache-token=Expires=1782807280~Starts=1782806980"
         "~URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20"
-        "~Signature=uQn5bEgN5NLSyoIOpfwn58A5pGVW9ZtR3cy93jKtGz03LlvFVh52HfHHXvbvdWcs0_CuDWv4ohvphQ5fKa9SDg"
+        "~Signature=M8DNdD5DicqR7rW_4bpCEbSFBk3VpvzrEp3DXKgz6FfZMH6UMLfoDnJ82IujbpWlnwOBx33vW5SfDDUcVtISAw"
+    )
+    # 2. The stream configuration
+    channel_name = "FIFA Live 6 (2026)"
+    original_url = (
+        "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-2/index.m3u8"
+        "?edge-cache-token=Expires=1782807429~Starts=1782807129"
+        "~URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20"
+        "~Signature=y4Y79QMpN-9kfIfVjQM37T8g8rKfQjQvYUNXRDszedynIoBJ-zsYJyC9hLAvXpacv3HNm6PuJVvpbLGHFjpWAA"
+    )
+    # 3. The stream configuration
+    channel_name = "FIFA Live 6 (2026)"
+    original_url = (
+        "https://prod-cdn01-live.toffeelive.com/live/FIFA-2026-3/index.m3u8"
+        "?edge-cache-token=Expires=1782807516~Starts=1782807216"
+        "~URLPrefix=aHR0cHM6Ly9wcm9kLWNkbjAxLWxpdmUudG9mZmVlbGl2ZS5jb20"
+        "~Signature=XwbyhHwv8PglbZVv8fqtAXqtLl73Y5m06H22a7xW6zyeSs9RDEGBpG12UO6jtuBFJnKTnaO7k_b2rPDBCjiNDw"
     )
 
     # Use the public mirror domain that bypasses localized DNS blocks
